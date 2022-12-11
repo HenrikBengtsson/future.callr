8ddbbc4a975b5f12602750d49535a57c67699cad
Henrik Bengtsson <hb@aroma-project.org>
2022-12-11 11:55:15 -0800

    TESTS: Test also run() + make sure to finish futures

--- tests/rng.R
+++ tests/rng.R
@@ -6,6 +6,26 @@ message("*** RNG ...")
 
 plan(callr, workers = 2L)
 
+message("- run() does not update RNG state")
+
+f1 <- future(1, lazy = TRUE)
+f2 <- future(2, lazy = TRUE)
+
+rng0 <- globalenv()$.Random.seed
+
+f1 <- run(f1)
+stopifnot(identical(globalenv()$.Random.seed, rng0))  ## RNG changed?
+
+f2 <- run(f2)
+stopifnot(identical(globalenv()$.Random.seed, rng0))  ## RNG changed?
+
+v1 <- value(f1)
+stopifnot(identical(v1, 1))
+
+v2 <- value(f2)
+stopifnot(identical(v2, 2))
+
+
 message("- future() does not update RNG state")
 
 rng0 <- globalenv()$.Random.seed
@@ -15,6 +35,12 @@ stopifnot(identical(globalenv()$.Random.seed, rng0))  ## RNG changed?
 
 f2 <- future(2)
 stopifnot(identical(globalenv()$.Random.seed, rng0))  ## RNG changed?
+
+v1 <- value(f1)
+stopifnot(identical(v1, 1))
+
+v2 <- value(f2)
+stopifnot(identical(v2, 2))
 
 
 message("- resolved() does not update RNG state")
@@ -27,6 +53,12 @@ stopifnot(identical(globalenv()$.Random.seed, rng0))  ## RNG changed?
 
 d2 <- resolved(f2)
 stopifnot(identical(globalenv()$.Random.seed, rng0))  ## RNG changed?
+
+v1 <- value(f1)
+stopifnot(identical(v1, 1))
+
+v2 <- value(f2)
+stopifnot(identical(v2, 2))
 
 
 message("- result() does not update RNG state")
@@ -41,6 +73,12 @@ stopifnot(identical(globalenv()$.Random.seed, rng0))  ## RNG changed?
 r2 <- result(f2)
 stopifnot(identical(r2$value, 2))
 stopifnot(identical(globalenv()$.Random.seed, rng0))  ## RNG changed?
+
+v1 <- value(f1)
+stopifnot(identical(v1, 1))
+
+v2 <- value(f2)
+stopifnot(identical(v2, 2))
 
 
 message("- value() does not update RNG state")
