4f40150af897c7f0bb7db8f7affa388e9b9cc3e4
Henrik Bengtsson <hb@aroma-project.org>
2023-01-03 15:15:59 -0800

    Declare import

--- R/utils,immediateConditions.R
+++ R/utils,immediateConditions.R
@@ -3,6 +3,7 @@ tmpl_expr_send_immediateConditions_via_file <- NULL
 readImmediateConditions <- function(...) NULL
 signalImmediateConditions <- function(...) NULL
 
+#' @importFrom utils packageVersion
 import_immediateConditions <- function() {
   if (packageVersion("future") < "1.30.0-9005") return()
   tmpl_expr_send_immediateConditions_via_file <<- import_future("tmpl_expr_send_immediateConditions_via_file", mode = "list")
