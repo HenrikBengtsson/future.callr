5068501b76ffa1ccc9cfc6762579f2e244bd7ac3
Henrik Bengtsson <hb@aroma-project.org>
2023-01-03 15:06:58 -0800

    Extract R/immediateConditions.R from R/zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,3 @@
-## immediateCondition related imports
-tmpl_expr_send_immediateConditions_via_file <- NULL
-readImmediateConditions <- function(...) NULL
-signalImmediateConditions <- function(...) NULL
-
-import_immediateConditions <- function() {
-  if (packageVersion("future") < "1.30.0-9005") return()
-  tmpl_expr_send_immediateConditions_via_file <<- import_future("tmpl_expr_send_immediateConditions_via_file", mode = "list")
-  readImmediateConditions <<- import_future("readImmediateConditions")
-  signalImmediateConditions <<- import_future("signalImmediateConditions")
-}
-
-
 ## To be cached by .onLoad()
 FutureRegistry <- NULL
 assertOwner <- NULL
